751879c2fc63f87f2e7e0037ab8854219a7bd545
muhdamir <amirulwasim96@gmail.com>
2023-11-04 20:13:37 +0800

    fix requirements and update code

--- src/application/services/fund_service.py
+++ src/application/services/fund_service.py
@@ -1,6 +1,6 @@
 from fastapi import Depends
 
-from persistence.fund_repository import FundRepository
+from persistence import FundRepository, FundManagerRepository
 
 from ..exceptions import EntryNotFoundError
 from ..models import FundPatchModel, FundPostModel
@@ -15,6 +15,10 @@ class FundService(
         repository: FundRepository = Depends(),
     ) -> None:
         self.repository = repository
+        # additional repository
+        self.fund_manager_repository = FundManagerRepository(
+            session=self.repository.session
+        )
 
     def get_by_id(self, id: int):
         if entry := self.repository.get_by_id(id=id):
@@ -25,14 +29,19 @@ class FundService(
         return self.repository.get_all()
 
     def create(self, data: FundPostModel):
-        dict_data = data.model_dump()
-        return self.repository.create(data=dict_data)
+        # check fund manager id exists
+        if self.fund_manager_repository.get_by_id(id=data.fund_manager_id):
+            dict_data = data.model_dump()
+            return self.repository.create(data=dict_data)
+        raise EntryNotFoundError(id=data.fund_manager_id)
 
     def update(self, id: int, data: FundPatchModel):
+        # check if id exists
         self.get_by_id(id=id)
         dict_data = data.model_dump(exclude_unset=True)
         return self.repository.update(id=id, data=dict_data)
 
     def delete(self, id: int) -> bool:
+        # check if id exists
         self.get_by_id(id=id)
         return self.repository.delete(id=id)
